06b1bb5a05577b08e36f4e8bb3a2238f1c30fd74
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-25 17:07:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(-2)
 }
 
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
 function createFareMultiplier(multiplier) {
   return function(fare){ fare * multiplier};
