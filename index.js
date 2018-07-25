94518255b5fa4824025637b552549495fe259fe4
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-25 17:07:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(-2)
 }
 
-const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()];
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 
 function createFareMultiplier(multiplier) {
